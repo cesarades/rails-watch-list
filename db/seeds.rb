8147a55dd3935dd6787774ea57ce7c8cd4694fc6
Cesar Ades <kito@ades.as>
2021-08-19 15:39:55 +0100

    Controller added: controllers and views doen (MVP)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning db"
+Movie.delete_all
+puts "Movies cleaned"
+
+5.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.famous_last_words,
+    poster_url: 'https://source.unsplash.com/random'
+  )
+  print "Adding movie #{movie.title}... "
+  print "#{movie.save}\n"
+end
+
+puts "Successfuly added #{Movie.count} movies to the database"
