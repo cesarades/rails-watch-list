c53e9b972119a48f985a8926abf301448b170730
Cesar Ades <kito@ades.as>
2021-08-20 11:10:58 +0100

    Root route and autocomplete structure

---
+++ app/javascript/packs/autocomplete.js
@@ -0,0 +1,31 @@
+// The API for autocomplete
+const url = "https://wagon-dictionary.herokuapp.com/autocomplete/";
+
+// Select the unordered list and create functions to add/remove li's
+const ul = document.getElementById("results");
+const deleteAll = elements => elements.forEach(element => element.remove());
+const addChildren = (parent, children) => children.forEach(child => parent.insertAdjacentHTML("beforeend", `<li>${child}</li>`));
+
+// This method adds the autocomplete for each keyUp
+const addSearchLetter = (event) => {
+  const searchTerm = event.currentTarget.value;
+  const lis = document.querySelectorAll("li");
+  if (searchTerm) {
+    const results = [];
+    fetch(url + searchTerm)
+      .then(response => response.json())
+      .then((data) => {
+        data.words.slice(0, 5).forEach(word => results.push(word));
+        console.log(results);
+        deleteAll(lis);
+        addChildren(ul, results);
+      });
+  } else {
+    deleteAll(lis);
+  }
+};
+
+// Select the input box and bind the keys to add a letter to search
+const input = document.getElementById("search_query");
+const bindKeyUp = element => element.addEventListener("keyup", addSearchLetter);
+bindKeyUp(input);
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[create]
   end
   resources :bookmarks, only: %i[destroy]
